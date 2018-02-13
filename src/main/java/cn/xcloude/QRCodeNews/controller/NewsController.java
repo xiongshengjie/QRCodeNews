58a41e74ec45aa575d0205a4414a55b205bc6450
Administrator <1145421439@qq.com>
2018-02-14 00:03:48 +0800

    测试项目

--- src/main/java/cn/xcloude/QRCodeNews/controller/NewsController.java
+++ src/main/java/cn/xcloude/QRCodeNews/controller/NewsController.java
@@ -4,6 +4,11 @@ package cn.xcloude.QRCodeNews.controller;
 import cn.xcloude.QRCodeNews.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author XiongShengjie
@@ -11,10 +16,18 @@ import org.springframework.stereotype.Controller;
  */
 
 @Controller
+@RequestMapping("/news")
 public class NewsController {
 
     @Autowired
     private NewsService newsService;
 
-
+    @RequestMapping("/add")
+    @ResponseBody
+    public Map<String,Object> addNews(){
+        Map<String,Object> result = new HashMap<String, Object>();
+        result.put("states",2000);
+        result.put("message","OK");
+        return result;
+    }
 }
