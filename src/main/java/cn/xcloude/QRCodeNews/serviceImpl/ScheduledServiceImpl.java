7cbb93a95a81b9c879b7ac960431d650a70aa4b8
xiongshengjie <1145421439@qq.com>
2023-04-08 12:39:50 +0800

    query 解析切换工具类

--- src/main/java/cn/xcloude/QRCodeNews/serviceImpl/ScheduledServiceImpl.java
+++ src/main/java/cn/xcloude/QRCodeNews/serviceImpl/ScheduledServiceImpl.java
@@ -9,7 +9,8 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.ImmutableSet;
-import org.apache.catalina.util.RequestUtil;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.util.TextUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,7 +22,11 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author XiongShengjie
@@ -100,18 +105,19 @@ public class ScheduledServiceImpl {
   }
 
   private String getRealUrl(String newsUrl) {
-    Map<String, String[]> values = new TreeMap<>();
+    List<NameValuePair> pairs = Collections.emptyList();
     try {
-      RequestUtil.parseParameters(values, new URL(newsUrl).getQuery(), "UTF-8");
-      String[] urls = values.get("id");
-      if (urls == null || urls.length < 1) {
-        return newsUrl;
-      }
-
-      return urls[0];
+      pairs = URLEncodedUtils.parse(new URL(newsUrl).getQuery(), StandardCharsets.UTF_8);
     } catch (MalformedURLException e) {
       logger.warn("parse url failed: {}", newsUrl);
     }
-    return newsUrl;
+
+    String url = pairs.stream().filter(pair -> "id".equals(pair.getName())).findAny()
+        .map(NameValuePair::getValue).orElse(null);
+    if (TextUtils.isEmpty(url)) {
+      return newsUrl;
+    }
+
+    return url;
   }
 }
